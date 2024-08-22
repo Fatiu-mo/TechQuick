78e33fd33a9459855a95190867c7b7ad63192934
Fatiu-mo <106868221+Fatiu-mo@users.noreply.github.com>
2024-08-22 13:07:11 +0100

    Tuning footer responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,50 +3,49 @@ import { CiFacebook, CiInstagram  } from "react-icons/ci";
 import { RiTwitterXFill } from "react-icons/ri";
 
 function Footer() {
-  return <div className="bg-[#FF6700]">
-    <footer className="py-32 px-6 md:grid md:grid-cols-3 font-poppins">
-    <div className="ml-28 py-14">
-      <p className="mb-3 font-bold text-lg text-gray-800 justify-center items-center">Techquick</p>
-      <p className="mb-2 text-gray-700 text-sm font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora soluta accusamus, eum et debitis provident quis esse molestiae dicta aliquam repudiandae? Perspiciatis maxime, incidunt dignissimos dicta aliquid aut corrupti nulla.</p>
-      <div className="flex gap-5 text-xl">
+
+  return <div className="md:grid grid-cols-4 gap-10 bg-[#FF6700] -mx-6 font-poppins p-20">
+    <div className="col-span-2 py-10 lg:pt-20 ">
+      <p className="font-bold text-lg text-gray-800">Techquick</p>
+      <p className="text-gray-700 text-sm font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora soluta accusamus, eum et debitis provident quis esse molestiae dicta aliquam repudiandae? Perspiciatis maxime, incidunt dignissimos dicta aliquid aut corrupti nulla.</p>
+      <div className="flex gap-4 text-xl mt-2">
         <RiTwitterXFill />
         <CiFacebook />
         <CiInstagram />
       </div>
     </div>
-    <div className="ml-28 flex flex-col gap-3">
-      <p className="font-bold text-lg text-gray-800">Hire A</p>
-      <ul className="flex flex-col gap-3 font-semibold text-sm text-amber-900">
+    <div>
+    <p className="font-bold text-lg text-gray-800 mb-3">Hire A</p>
+      <ul className="font-semibold text-sm text-amber-900 flex flex-col gap-3">
         <li>Website Developer</li>
         <li>App Developer</li>
         <li>Ui Ux Designer</li>
         <li>Social Media Marketer</li>
       </ul>
-      <p className="font-bold text-lg text-gray-800 mt-4">Pages</p>
-      <ul className="flex flex-col gap-3 font-semibold text-sm text-amber-900">
+      <p className="font-bold text-lg text-gray-800 mt-4 mb-3">Pages</p>
+      <ul className="font-semibold text-sm text-amber-900 flex flex-col gap-3">
         <li>Privacy Policy</li>
         <li>Disclaimer</li>
         <li>About Us</li>
         <li>Contact Us</li>
       </ul>
     </div>
-    <div className="ml-28 sm:mt-4 md:mt-0">
-      <p className="font-bold text-lg text-gray-800 mb-3">Buy From Us</p>
-      <ul className="flex flex-col gap-3 font-semibold text-sm text-amber-900">
+    <div className="">
+      <p className="font-bold text-lg text-gray-800 mt-4 mb-3">Buy From Us</p>
+      <ul className="font-semibold text-sm text-amber-900 flex flex-col gap-3">
         <li>Courses</li>
         <li>Php Scripts</li>
         <li>Ready Made Ui design template</li>
         <li>App source code</li>
       </ul>
-      <div className="mt-10">
-        <p className=" mb-3 font-medium text-gray-950">Hire Techquick For A Project</p>
-        <button className="bg-[#512609] text-white w-fit px-12 py-2 ml-8">Lets Go</button>
+      <div className="mt-12">
+        <p className="font-medium text-[#292727] mb-3">Hire Techquick For A Project</p>
+        <div className="md:flex justify-center">
+        <button className="flex items-center justify-center bg-[#512609] text-center font-semibold text-white w-fit px-9 py-2 whitespace-nowrap">Lets Go</button>
+        </div>
       </div>
-
     </div>
-  </footer>
   </div>
-   
 }
 
 export default Footer;
